f636b924ccfdb742b4d885f781c34cfc11260f64
MANOJ-S-S <ssmanoj96@gmail.com>
2019-07-30 21:12:11 +0530

    commited

--- app/src/main/java/com/example/reviewfeed/Main2Activity.java
+++ app/src/main/java/com/example/reviewfeed/Main2Activity.java
@@ -6,6 +6,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.view.MenuItem;
@@ -13,6 +15,7 @@ import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
     private TextView mTextMessage;
+    Fragment fragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -22,16 +25,18 @@ public class Main2Activity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_dashboard:
 
-                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-
-// Replace whatever is in the fragment_container view with this fragment,
-// and add the transaction to the back stack so the user can navigate back
-                    transaction.replace(R.id.frame, new DashboardFrag());
-                  //  transaction.addToBackStack(null);
-
-// Commit the transaction
-                    transaction.commit();
+                    replaceFragment(new DashboardFrag());
                     return true;
+
+                case R.id.navigation_forms:
+                    return true;
+
+                case R.id.navigation_training:
+                    return true;
+
+                case R.id.navigation_events:
+                    return true;
+
 
             }
             return false;
@@ -46,5 +51,14 @@ public class Main2Activity extends AppCompatActivity {
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+    public void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame, fragment);
+        fragmentTransaction.addToBackStack(fragment.toString());
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        fragmentTransaction.commit();
+    }
+
 
 }
